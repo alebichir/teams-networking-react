a044b280089e5de3e8f14f409a47eda305b4786f
Alexandra Bichir <alexandrabichir@yahoo.com>
2023-08-09 21:51:03 +0300

    simple map teams to html rows

--- src/teams/TeamsTable.tsx
+++ src/teams/TeamsTable.tsx
@@ -1,4 +1,98 @@
+function TeamRow(team) {
+  const { id, url } = team;
+  const displayUrl = url.startsWith("https://github.com/") ? url.substring(19) : url;
+  return (
+    <tr>
+      <td style={{ textAlign: "center" }}>
+        <input type="checkbox" name="selected" value="${id}" />
+      </td>
+      <td>
+        <span
+          className="circle-bullet"
+          style={{
+            background: stringToColour(team.promotion)
+          }}
+        ></span>
+        {team.promotion}
+      </td>
+      <td>{team.members}</td>
+      <td>{team.name}</td>
+      <td>
+        <a href="${url}" target="_blank">
+          {displayUrl}
+        </a>
+      </td>
+      <td>
+        <button type="button" data-id="${id}" className="action-btn edit-btn">
+          &#9998;
+        </button>
+        <button type="button" data-id="${id}" className="action-btn delete-btn">
+          ♻
+        </button>
+      </td>
+    </tr>
+  );
+}
+
+function stringToColour(str) {
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  var colour = "#";
+  for (var i = 0; i < 3; i++) {
+    var value = (hash >> (i * 8)) & 0xff;
+    colour += ("00" + value.toString(16)).substr(-2);
+  }
+  return colour;
+}
+
 export function TeamsTable() {
+  const teams = [
+    {
+      id: "toze8j1610313009673",
+      promotion: "html",
+      members: "Nicolae Matei, HTML",
+      name: "Web Presentation",
+      url: "https://github.com/nmatei/web-intro-presentation",
+      createdBy: "nmatei"
+    },
+    {
+      id: "ezabnf1630345987541",
+      promotion: "css",
+      members: "Nicolae",
+      name: "Names",
+      url: "https://github.com/nmatei/nmatei.github.io",
+      createdBy: "nmatei"
+    },
+    {
+      id: "86mq81630347385708",
+      promotion: "js",
+      members: "Matei, Andrei",
+      name: "JS/HTML/CSS Quiz",
+      url: "https://github.com/nmatei/simple-quiz-app",
+      createdBy: "nmatei"
+    },
+    {
+      id: "w2aal1630347411901",
+      promotion: "js",
+      members: "FastTrackIT Students, Nicolae",
+      name: "Teams Networking",
+      url: "https://github.com/nmatei/teams-networking",
+      createdBy: "nmatei"
+    },
+    {
+      id: "w2aal1630347411902",
+      promotion: "react",
+      members: "FastTrackIT Students, Nicolae",
+      name: "Teams Networking React",
+      url: "https://github.com/nmatei/teams-networking-react",
+      createdBy: "nmatei"
+    }
+  ];
+
+  console.info("render table", teams);
+
   return (
     <form id="teamsForm" action="" method="get">
       <table id="teamsTable">
@@ -23,7 +117,7 @@ export function TeamsTable() {
           </tr>
         </thead>
 
-        <tbody className="context"></tbody>
+        <tbody className="context">{teams.map(team => TeamRow(team))}</tbody>
 
         <tfoot>
           <tr>

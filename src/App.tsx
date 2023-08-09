3674e8ada94c67f1fb22229db94b2d414410ba38
Alexandra Bichir <alexandrabichir@yahoo.com>
2023-08-09 20:34:05 +0300

    add content weapper and toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -36,8 +36,24 @@ function AppHeader() {
   );
 }
 
+function Toolbar() {
+  return (
+    <div className="toolBar">
+      <button id="removeSelected">❌Remove selected</button>
+      <div className="toolBarFill"></div>
+      <input name="search" placeholder="Search" id="search" />
+      <label htmlFor="search">🔎</label>
+    </div>
+  );
+}
+
 function ContentWrapper() {
-  return <section id="main">...</section>;
+  return (
+    <section id="main">
+      <Toolbar />
+      TODO table ...
+    </section>
+  );
 }
 
 function AppFooter() {

2aca92a05130abc6191668ea80dd5e3a534cc0ee
Alexandra Bichir <alexandrabichir@yahoo.com>
2023-08-11 19:27:40 +0300

    render only one table

--- src/teams/TeamsTable.tsx
+++ src/teams/TeamsTable.tsx
@@ -125,17 +125,11 @@ export function TeamsTable(props: Props) {
 }
 
 export function TeamsTableWrapper() {
-  let teams: Team[] = [];
+  let teams: Team[] = []; // don't know where to take them 💫
 
   return (
     <>
-      <TeamsTable loading={true} teams={[]} />
-      <br />
-      <TeamsTable loading={false} teams={[]} />
-      <br />
       <TeamsTable loading={true} teams={teams} />
-      <br />
-      <TeamsTable loading={false} teams={teams} />
     </>
   );
 }

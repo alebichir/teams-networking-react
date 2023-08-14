9b481c62447b8247c5e7134ced0f4d2eaa4fca34
Alexandra Bichir <alexandrabichir@yahoo.com>
2023-08-14 17:56:22 +0300

    useState hook for search,createdBy and ts examples

--- src/main/components.tsx
+++ src/main/components.tsx
@@ -1,19 +1,37 @@
+import { useState } from "react";
 import { TeamsTableWrapper } from "../teams/TeamsTable";
 
-const Toolbar = () => (
-  <div className="toolBar">
-    <button id="removeSelected">❌Remove selected</button>
-    <div className="toolBarFill"></div>
-    <input name="search" placeholder="Search" id="search" />
-    <label htmlFor="search">🔎</label>
-  </div>
-);
-
 export function ContentWrapper() {
+  //console.warn("wrapper.render");
+  //let search = "";
+  const [search, setSearch] = useState("");
+  //const r = useState("");
+  // const search1 = r[0];
+  // const setSearch1 = r[1];
+
+  //console.warn("wrapper.render %o", search);
+
   return (
-    <section id="main">
-      <Toolbar />
-      <TeamsTableWrapper search="" />
-    </section>
+    <div id="main">
+      <div className="tbar">
+        <button id="removeSelected">✖ Remove selected</button>
+        <div className="tfill"></div>
+        <input
+          type="search"
+          name="search"
+          id="searchTeams"
+          placeholder="Search..."
+          onChange={e => {
+            console.info("search", e.target.value);
+            //search = e.target.value;
+            setSearch(e.target.value);
+            //ContentWrapper(); //not working
+          }}
+        />
+        <label htmlFor="searchTeams">🔎</label>
+      </div>
+
+      <TeamsTableWrapper search={search} />
+    </div>
   );
 }
---
+++ src/teams/examples.ts
@@ -0,0 +1,30 @@
+import { Team } from "./models";
+
+let firstName = "yes";
+// firstName = false;
+// conf.ready = "not ready";
+
+export default null;
+
+type Store<T = any> = {
+  find<K extends keyof T>(fieldName: K, value: T[K]): T;
+  update(id: string, values: Partial<T>): void;
+};
+
+const store: Store<Team> = {
+  find(fieldName, values) {
+    return {
+      id: "1",
+      members: "",
+      name: "",
+      promotion: "",
+      url: ""
+    };
+  },
+  update(id, value) {}
+};
+
+const t1 = store.find("name", "react");
+//const t2 = store.find("amount", 2);
+
+//store.update("123", { amount: 123 });
